5149e42dfd15c8a638978ed7abea993d82066c47
jjcarreonc-droid <280094489+jjcarreonc-droid@users.noreply.github.com>
2026-04-28 17:59:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Mi primera app 🚀")
+
+st.write("Hola Jose, ya funciona!")
+
+numero = st.slider("Elige un número", 0, 100)
+
+st.write("Elegiste:", numero)
